d1a2abc5fbbc1ac9621b237a827d9447bdf23230
Mujtaba <mujtabarasheed20@gmail.com>
2026-07-07 16:13:40 +0500

    update

---
+++ app/Provider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './_components/Navbar'
+
+const Provider = ({children}) => {
+  return (
+    <div>
+        <Navbar/>
+        {children}</div>
+  )
+}
+
+export default Provider
